45c57e26c0f3a3962dc68507aa1a58c6db38db53
Kazi Tanjim Shakib <kazitanjimshakib053@gmail.com>
2026-05-03 18:23:53 +0600

    Initial commit

---
+++ SampleFirebaseAuthentication/SampleFirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFirebaseAuthenticationApp.swift
+//  SampleFirebaseAuthentication
+//
+//  Created by Kazi Tanjim Shakib on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleFirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
